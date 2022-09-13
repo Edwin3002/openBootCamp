2a4942f66064d59cfd397356bb8d4cdc090737a7
Edwin3002 <vargas11742@gmail.com>
2022-09-12 19:27:57 -0500

    h1

---
+++ introducción/herencia.java
@@ -0,0 +1,26 @@
+public class herencia {
+    public static void main(String[] args) {
+        CocheElectrico.cocheElectrico = new CocheElectrico();
+        cocheElectrico.velocidadMax = 14;
+        cocheElectrico.matricula = "ABC" ;
+    }
+}
+
+class Vehiculo {
+    int velocidadMax;
+    String matricula;
+
+    public boolean compruebaMatricula(String matricula){
+        if(matricula == "XXX"){
+            return false;
+        }
+    }
+}
+
+class Coche extends Vehiculo {
+
+}
+
+class CocheElectrico extends Coche {
+
+}
